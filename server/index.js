276cfb11046cdb558ecf6dbcf4709c33719661e7
Anis Safine Laget <anis@users.noreply.github.com>
2019-03-13 10:30:47 +0100

    (tech) Rename all controllers

--- server/index.js
+++ server/index.js
@@ -4,11 +4,11 @@ const { sequelize } = require('../db/models');
 const dataAccess = require('#server/dataAccess')(sequelize);
 
 const userController = require('#server/controllers/userController');
-const authController = require('#server/auth');
-const configController = require('#server/controllers/config');
+const authController = require('#server/controllers/authController');
+const configController = require('#server/controllers/configController');
 const townController = require('#server/controllers/townController')(dataAccess);
-const actionController = require('#server/controllers/actions');
-const geoController = require('#server/controllers/geo');
+const actionController = require('#server/controllers/actionController');
+const geoController = require('#server/controllers/geoController');
 
 const app = require('#server/app')({
     user: userController,
