489347702058dfba99416f3e2786de8232549484
Anis Safine Laget <anis@users.noreply.github.com>
2019-06-13 17:38:34 +0200

    (tech) remove useless test functions

--- test/bootstrap/index.js
+++ test/bootstrap/index.js
@@ -3,8 +3,6 @@ require('module-alias/register');
 require('./custom-hooks');
 require('./random');
 
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
 
 global.db = () => new Sequelize({
@@ -59,41 +57,4 @@ global.insertFixtures = async function insertFixtures(db, inputs) {
         /* eslint-disable-next-line */
         await insertIntoDb(db, inputs[i].table, inputs[i].rows);
     }
-};
-
-/**
- * Generates the name of a class testsuite
- *
- * @param {string} mainTestFile Absolute path to a class's main test file
- *
- * @returns {string}
- */
-global.testName = function testName(mainTestFile) {
-    const dirname = path.dirname(mainTestFile);
-    const baseName = path.basename(mainTestFile);
-
-    return `${dirname.split('/suites/')[1]}/${baseName.split('.')[0]}`;
-};
-
-/**
- * Loads all the sub-tests of a testsuite
- *
- * Every tested class (say, "C") is assumed to have its own test directory, inside which
- * should be found:
- * - a file C.spec.js, which declares the testsuite for this class
- * - a set of files C.*.spec.js, which each represent a single method of that class, and the
- *   declare the related sub-tests
- *
- * This method takes the path to a class's test directory, and requires each sub-test file.
- *
- * @param {string} directory Absolute path to a class's test directory
- */
-global.loadSuite = function loadSuite(directory) {
-    fs
-        .readdirSync(directory)
-        .filter(file => (file.indexOf('.') !== 0) && /^([^.]+\.){2}spec\.js$/gi.test(file) === true)
-        .forEach((file) => {
-            /* eslint-disable-next-line */
-            require(path.join(directory, file))();
-        });
 };
