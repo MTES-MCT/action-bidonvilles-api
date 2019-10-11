7e20417e191fc6df9c6f6f453b55c1fd32c50433
Anis Safine Laget <anis@users.noreply.github.com>
2019-10-11 19:41:51 +0200

    Wording for changelog 0.0.75

--- db/seeders/000017-changelogs.js
+++ db/seeders/000017-changelogs.js
@@ -1,27 +1,27 @@
-const { backUrl } = require('../../server/config');
+const { frontUrl, backUrl } = require('../../server/config');
 
 const changelog = {
     app_version: '0.0.75',
     date: new Date(2019, 9, 1),
     items: [
         {
-            title: 'Mobiliser plus d’acteurs grâce aux accès différenciés',
-            description: '<p>À chaque acteur, ses droits d’accès.</p><p>Tout acteur de la résorption des bidonvilles peut effectuer une demande d’accès. Désormais, l’administrateur local identifié parmi les correspondants valide et paramètre l\'accès du nouvel utilisateur.</p><p><a href="http://localhost:1234/#/typologie-des-acces" target="_blank">Guide des accès et de l\'administrateur</a></p>',
+            title: 'Mise en place des accès différenciés pour les différents types d\'utilisateurs',
+            description: `<p>À chaque acteur, ses droits d'accès.</p><p>Tout acteur de la résorption des bidonvilles peut effectuer une demande d'accès. Désormais, l'administrateur local identifié parmi les correspondants valide et paramètre l'accès du nouvel utilisateur.</p><p><a href="${frontUrl}/typologie-des-acces" target="_blank">Guide des accès et de l'administrateur</a></p>`,
             image: `${backUrl}/assets/changelog/0.0.75/item_1.jpg`,
         },
         {
-            title: 'Voir rapidement la situation des sites sur un territoire',
-            description: '<p><br/>Le tableau récapitulatif des sites a été optimisé pour mieux visualiser la situation. Vous pouvez le retrouver dans l’onglet site.</p>',
+            title: 'Amélioration de l\'affichage de la liste de sites',
+            description: '<p>Le tableau récapitulatif des sites a été optimisé pour mieux visualiser la situation. Vous pouvez le retrouver dans l\'onglet site.</p>',
             image: `${backUrl}/assets/changelog/0.0.75/item_2.jpg`,
         },
         {
-            title: 'Exporter un tableau Excel lisible et exploitable',
-            description: '<p><br/>Vous pouvez plus facilement exploiter les données grâce à l’optimisation de la fonctionnalité d’export des sites.</p><p><br/>Les sites fermés et ouverts ont été différenciés pour un suivi plus adapté. L’export s’effectue depuis la liste des sites.</p>',
+            title: 'Amélioration de l\'export d\'un tableau Excel lisible et exploitable',
+            description: '<p>Vous pouvez plus facilement exploiter les données grâce à l\'optimisation de la fonctionnalité d\'export des sites. Les sites fermés et ouverts ont été différenciés pour un suivi plus adapté. L\'export s\'effectue depuis la liste des sites.</p>',
             image: `${backUrl}/assets/changelog/0.0.75/item_3.jpg`,
         },
         {
-            title: 'Contacter les partenaires des actions de résorption des bidonvilles',
-            description: '<p><br/>Un annuaire avec l’ensemble des utilisateurs est disponible depuis le menu principal.</p>',
+            title: 'Mise en place d\'un annuaire des partenaires des actions de résorption',
+            description: '<p>Un annuaire avec l\'ensemble des utilisateurs est disponible depuis le menu principal.</p>',
             image: `${backUrl}/assets/changelog/0.0.75/item_4.jpg`,
         },
     ],
