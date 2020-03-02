2e49f7178b2281ffbb79a789e1cdf7ff91e193b7
Anis Safine Laget <anis@users.noreply.github.com>
2020-03-02 18:08:37 +0100

    Modale sur le coronavirus

--- db/seeders/000017-changelogs.js
+++ db/seeders/000017-changelogs.js
@@ -1,13 +1,13 @@
 const { frontUrl, backUrl } = require('../../server/config');
 
 const changelog = {
-    app_version: '0.2.0',
-    date: new Date(2020, 1, 3),
+    app_version: '0.3.3',
+    date: new Date(2020, 2, 2),
     items: [
         {
-            title: 'Vous pouvez renseigner les dispositifs existants en 2019 et leurs résultats jusqu\'au 21/02/2020',
-            description: '<p><ul><li>Le <strong>service de l’Etat</strong> en charge du pilotage d’un ou plusieurs dispositifs en déclare l’existence.</li><li>L’<strong>opérateur associatif</strong> qui intervient renseigne les indicateurs de suivi présents sur la plateforme.</li></ul></p><p>Nous vous invitons désormais à mettre à jour au fil de l’eau ces informations.</p>',
-            image: `${backUrl}/assets/changelog/0.2.0/item_1.png`,
+            title: 'Recommandations à destination des services en charge du pilotage de la résorption des bidonvilles et des associations y intervenant',
+            description: `<p>Vous trouverez ci-dessous une fiche présentant les <strong>préconisations officielles du ministère de la santé pour les professionnels, personnes accompagnées et proches.</strong> Elle a été transmise aux fédérations et associations gestionnaires qui accueillent des personnes sans domicile.</p><p>Concernant le public en bidonvilles, nous vous recommandons une attention particulière, notamment :<ul><li>relayer les mesures de sensibilisation, d’information et d’orientation des personnes vulnérables auprès de vos partenaires et des habitants des bidonvilles, en lien avec les services de préfecture et les agences régionales de santé</li><li>assurer une intervention renforcée des équipe d’intervention sociale et de médiation sanitaire sur les sites</li><li>mobiliser lorsque nécessaire, des supports traduits des recommandations destinées au grand public, de même que des services d’interprétariat</li></ul></p><p>En cas de question, merci d’interroger rapidement la <strong>cellule de crise Covid19 de la DGCS</strong> à l’adresse suivante : dgcs-alerte-covid@social.gouv.fr</p><p>Merci de transmettre à la Dihal toute information utile, y compris des documents traduits que vous diffuseriez.</p><p><a href="${backUrl}/assets/informations-nouveau-coronavirus.pdf" target="_blank">Informations, et recommandations sur le nouveau Coronavirus</a></p>`,
+            image: `${backUrl}/assets/changelog/0.3.3/item_1.png`,
         },
     ],
 };
