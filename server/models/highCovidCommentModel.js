c7de5e16341aa1b791c20085f07ede86913273bf
Anis Safine Laget <anis@users.noreply.github.com>
2020-03-26 11:29:00 +0100

    Création de highCovidCommentModel.create()

--- server/models/highCovidCommentModel.js
+++ server/models/highCovidCommentModel.js
@@ -100,5 +100,53 @@ module.exports = (database) => {
         return comments;
     };
 
+    methods.create = async (user, data) => {
+        const transaction = await database.transaction();
+
+        // comment
+        const [[{ id }]] = await database.query(
+            `INSERT INTO
+                high_covid_comments(
+                    description,
+                    created_by
+                )
+                
+            VALUES
+                (
+                    :description,
+                    :userId
+                )
+            
+            RETURNING high_covid_comment_id AS id`,
+            {
+                replacements: {
+                    description: data.description,
+                    userId: user.id,
+                },
+                transaction,
+            },
+        );
+
+        // territories
+        const query = data.departements.map(() => '(?, ?, ?)').join(', ');
+        const values = data.departements.reduce((acc, code) => [...acc, id, code, user.id], []);
+
+        return database.query(
+            `INSERT INTO
+                high_covid_comment_territories(
+                    fk_comment,
+                    fk_departement,
+                    created_by
+                )
+                
+            VALUES
+                ${query}`,
+            {
+                replacements: values,
+                transaction,
+            },
+        );
+    };
+
     return methods;
 };
