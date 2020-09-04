71c899b5d17427ca3e24518e50d257058c704d24
gdestrem <gael.destrem@gmail.com>
2020-09-04 13:56:14 +0200

    Sort by position on findAll methods + add field position on resources

--- server/models/fieldTypeModel.js
+++ server/models/fieldTypeModel.js
@@ -10,6 +10,7 @@ function serializeFieldType(fieldType) {
         id: fieldType.id,
         label: fieldType.label,
         color: `#${fieldType.color}`,
+        position: fieldType.position,
     };
 }
 
@@ -19,8 +20,10 @@ module.exports = database => ({
             `SELECT
                 field_types.field_type_id AS id,
                 field_types.label AS label,
-                field_types.color AS color
-            FROM field_types`,
+                field_types.color AS color,
+                field_types.position AS position
+            FROM field_types
+            ORDER BY position`,
             {
                 type: database.QueryTypes.SELECT,
             },
