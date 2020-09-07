3e7c1590dc098c74b819fa037cc50001279b70cd
gdestrem <gael.destrem@gmail.com>
2020-09-07 14:08:11 +0200

    Add uid to method serializeElectricityType

--- server/models/electricityTypeModel.js
+++ server/models/electricityTypeModel.js
@@ -10,6 +10,7 @@ function serializeElectricityType(electricityType) {
         id: electricityType.id,
         label: electricityType.label,
         position: electricityType.position,
+        uid: electricityType.uid,
     };
 }
 
@@ -19,7 +20,8 @@ module.exports = database => ({
             `SELECT
                 electricity_types.electricity_type_id AS id,
                 electricity_types.label AS label,
-                electricity_types.position AS position
+                electricity_types.position AS position,
+                electricity_types.uid AS uid
             FROM electricity_types
             ORDER BY position ASC`,
             {
