4b4414cc58f6fb93e265a2538d971f00623d44c4
gdestrem <gael.destrem@gmail.com>
2020-09-07 15:50:39 +0200

    Fix migration scripts to use label instead of ids

--- db/migrations/000096-02-alter-table-electricity_types.js
+++ db/migrations/000096-02-alter-table-electricity_types.js
@@ -1,41 +1,44 @@
-
+const yesLabel = 'Oui';
+const noLabel = 'Non';
+const unknownLabel = 'Inconnu';
 
 module.exports = {
-    up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
-        transaction => queryInterface.sequelize.queryInterface.addColumn(
-            'electricity_types',
-            'uid',
-            {
-                type: Sequelize.STRING,
-                allowNull: true,
-            },
-        ).then(() => queryInterface.sequelize.query(
-            'UPDATE electricity_types SET uid = :value where electricity_type_id = 1',
-            {
-                transaction,
-                replacements: { value: 'inconnu' },
-            },
-        )).then(() => queryInterface.sequelize.query(
-            'UPDATE electricity_types SET uid = :value where electricity_type_id = 2',
-            {
-                transaction,
-                replacements: { value: 'non' },
-            },
-        )).then(() => queryInterface.sequelize.query(
-            'UPDATE electricity_types SET uid = :value where electricity_type_id = 3',
-            {
-                transaction,
-                replacements: { value: 'oui' },
-            },
-        )),
-    )
-        // Make UID mandatory
-        .then(() => queryInterface.changeColumn('electricity_types', 'uid', {
+    up: (queryInterface, Sequelize) => queryInterface.sequelize.queryInterface.addColumn(
+        'electricity_types',
+        'uid',
+        {
             type: Sequelize.STRING,
-            allowNull: false,
-        })),
+            allowNull: true,
+        },
+    ).then(() => queryInterface.sequelize.transaction(
+        transaction => Promise.all([
+            queryInterface.sequelize.query(
+                'UPDATE electricity_types SET uid = :value where label = :label',
+                {
+                    transaction,
+                    replacements: { value: 'inconnu', label: unknownLabel },
+                },
+            ),
+            queryInterface.sequelize.query(
+                'UPDATE electricity_types SET uid = :value where label = :label',
+                {
+                    transaction,
+                    replacements: { value: 'non', label: noLabel },
+                },
+            ),
+            queryInterface.sequelize.query(
+                'UPDATE electricity_types SET uid = :value where label = :label',
+                {
+                    transaction,
+                    replacements: { value: 'oui', label: yesLabel },
+                },
+            ),
 
-
+        ]),
+    ).then(() => queryInterface.changeColumn('electricity_types', 'uid', {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }))),
     down: queryInterface => queryInterface.sequelize.transaction(
         transaction => queryInterface.removeColumn('electricity_types', 'uid', { transaction }),
     ),
