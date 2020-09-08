108a8da08c7992379bf921ce95f8fc45afb80e54
Anis Safine Laget <anis@users.noreply.github.com>
2020-09-08 14:53:09 +0200

    Fix PR feedbacks
    - use a single transaction for all migration files
    - fix label of regular and irregular accesses
    - add a space after electricity comment

--- db/migrations/000096-02-alter-table-electricity_types-add-uid.js
+++ db/migrations/000096-02-alter-table-electricity_types-add-uid.js
@@ -3,45 +3,56 @@ const noLabel = 'Non';
 const unknownLabel = 'Inconnu';
 
 module.exports = {
-    up: (queryInterface, Sequelize) => queryInterface.sequelize.queryInterface.addColumn(
-        'electricity_types',
-        'uid',
-        {
-            type: Sequelize.STRING,
-            allowNull: true,
-        },
-    ).then(() => queryInterface.sequelize.transaction(
-        transaction => Promise.all([
-            queryInterface.sequelize.query(
-                `UPDATE electricity_types SET uid = :value where label = :label`,
+    up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
+        transaction => queryInterface.addColumn(
+            'electricity_types',
+            'uid',
+            {
+                type: Sequelize.STRING,
+                allowNull: true,
+            },
+            {
+                transaction,
+            },
+        )
+            .then(() => Promise.all([
+                queryInterface.sequelize.query(
+                    'UPDATE electricity_types SET uid = :value where label = :label',
+                    {
+                        transaction,
+                        replacements: { value: 'inconnu', label: unknownLabel },
+                    },
+                ),
+                queryInterface.sequelize.query(
+                    'UPDATE electricity_types SET uid = :value where label = :label',
+                    {
+                        transaction,
+                        replacements: { value: 'non', label: noLabel },
+                    },
+                ),
+                queryInterface.sequelize.query(
+                    'UPDATE electricity_types SET uid = :value where label = :label',
+                    {
+                        transaction,
+                        replacements: { value: 'oui', label: yesLabel },
+                    },
+                ),
+            ]))
+            .then(() => queryInterface.changeColumn(
+                'electricity_types',
+                'uid',
+                {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
                 {
                     transaction,
-                    replacements: { value: 'inconnu', label: unknownLabel },
                 },
-            ),
-            queryInterface.sequelize.query(
-                'UPDATE electricity_types SET uid = :value where label = :label',
-                {
-                    transaction,
-                    replacements: { value: 'non', label: noLabel },
-                },
-            ),
-            queryInterface.sequelize.query(
-                'UPDATE electricity_types SET uid = :value where label = :label',
-                {
-                    transaction,
-                    replacements: { value: 'oui', label: yesLabel },
-                },
-            ),
+            )),
+    ),
 
-        ]),
-    ).then(() => queryInterface.changeColumn('electricity_types', 'uid', {
-        type: Sequelize.STRING,
-        allowNull: false,
-    }))),
     down: queryInterface => queryInterface.sequelize.transaction(
         transaction => queryInterface.removeColumn('electricity_types', 'uid', { transaction }),
     ),
-
 
 };
