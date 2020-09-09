ab8ed8ea86667048e9f638f9dce9e60ea0e68f2e
Anis Safine Laget <anis@users.noreply.github.com>
2020-09-09 10:38:30 +0200

    (556) Fix PR: fix badly formed SQL query

--- server/models/ownerTypeModel.js
+++ server/models/ownerTypeModel.js
@@ -15,6 +15,7 @@ module.exports = database => ({
             `SELECT
                 owner_types.owner_type_id AS id,
                 owner_types.label AS label,
+                owner_types.position AS position
             FROM owner_types
             WHERE owner_types.owner_type_id = :id
             ORDER BY position ASC`,
