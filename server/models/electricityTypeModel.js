1b3e3b01d8375f7c64c61164b9412ef99c1b9de2
Anis Safine Laget <anis@users.noreply.github.com>
2020-10-21 10:45:36 +0200

    (tech/cypress) utilisation d'express-validator pour towns.create et towns.edit
    - suppression de tout le code devenu inutile (valideInput, une grosse partie de cleanParams)
    - ajout de quelques méthodes utilitaires dans la couche modèle (findOne, find, nécessaires pour la validation de certaines données comme le type d'électricité)
    - amélioration des critères de validation (notamment sur les dates)

--- server/models/electricityTypeModel.js
+++ server/models/electricityTypeModel.js
@@ -31,4 +31,28 @@ module.exports = database => ({
 
         return electricityTypes.map(serializeElectricityType);
     },
+
+    findOne: async (id) => {
+        const electricityTypes = await database.query(
+            `SELECT
+                electricity_types.electricity_type_id AS id,
+                electricity_types.label AS label,
+                electricity_types.position AS position,
+                electricity_types.uid AS uid
+            FROM electricity_types
+            WHERE electricity_types.electricity_type_id = :id`,
+            {
+                type: database.QueryTypes.SELECT,
+                replacements: {
+                    id,
+                },
+            },
+        );
+
+        if (electricityTypes.length !== 1) {
+            return null;
+        }
+
+        return serializeElectricityType(electricityTypes[0]);
+    },
 });
