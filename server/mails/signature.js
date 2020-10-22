821b6f5b4e6a1468459de381d7741443608a9d44
Anis Safine Laget <anis@users.noreply.github.com>
2020-10-22 16:23:18 +0200

    (581) Mise à jour de la signature mail

--- server/mails/signature.js
+++ server/mails/signature.js
@@ -1,40 +1,23 @@
 const { backUrl } = require('#server/config');
 
 module.exports = {
-    TextPart: `L'équipe de Résorption Bidonvilles
-    -
-    Connaître, partager, agir
-    pour résorber les bidonvilles
+    TextPart: `Résorption-bidonvilles
+    Agir pour résorber les bidonvilles
     contact@resorption-bidonvilles.gouv.fr
-    -
-    La plateforme Résorption bidonvilles est développée par
-    la Délégation Interministérielle à l'hébergement et l'accès au logement`,
 
-    HTMLPart: `<tr>
-        <td bgcolor="#ffffff">
-            <img src="${backUrl}/assets/logo_rb.png" /><br/>
-            <b color="red" style="font-weight: bold; color: red">Connaître, partager, agir</b><br/>
-            <b color="red" style="font-weight: bold; color: red">pour résorber les bidonvilles</b><br/>
-            <a href="mailto:contact@resorption-bidonvilles.beta.gouv.fr" color="red" style="color: red; text-decoration: none">contact@resorption-bidonvilles.beta.gouv.fr</a><br/>
-            <hr />
-        </td>
-    </tr>
-    <tr>
-        <td bgcolor="#ffffff">
-            <table border="0" cellpadding="0" cellspacing="0">
-                <tbody>
-                    <tr>
-                        <td bgcolor="#ffffff" valign="middle"><img src="${backUrl}/assets/logo_dihal.png" /></td>
-                        <td bgcolor="#ffffff" valign="middle"><img src="${backUrl}/assets/marianne.png" /></td>
-                    </tr>
-                </tbody>
-            </table>
-        </td>
-    </tr>
-    <tr>
-        <td bgcolor="#ffffff">
-            La plateforme Résorption bidonvilles est développée par<br/>
-            la Délégation Interministérielle à l'hébergement et l'accès au logement
-        </td>
-    </tr>`,
+    Développé par la Dihal`,
+
+    HTMLPart: `<td bgcolor="#ffffff">
+        <img src="${backUrl}/assets/mails/marianne.png" width="40" height="15"
+            style="width: 40px; height: 15px; margin-bottom: 8pt;" /><br />
+        <span
+            style=" font-weight: bold; font-style: italic; font-size: 10pt; color: #1C1C1A;">Résorption-bidonvilles</span><br />
+        <span style="font-size: 10pt; color: #1C1C1A;">Agir pour résorber les bidonvilles</span><br />
+        <a href="mailto:contact@resorption-bidonvilles.beta.gouv.fr"
+            style="color: #000F8C; text-decoration: none; font-size: 10pt;">contact@resorption-bidonvilles.beta.gouv.fr</a><br />
+        <br />
+        <span style="font-size: 8pt; color: #1C1C1A;">Développé par la <a
+                href="https://www.gouvernement.fr/resorption-des-bidonvilles"
+                style="color: #000F8C; text-decoration: none;">Dihal</a></span>
+    </td>`,
 };
