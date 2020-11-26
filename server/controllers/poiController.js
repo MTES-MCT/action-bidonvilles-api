53b6d3f4a064cca51969a32a64e01373133489e5
gdestrem <gael.destrem@gmail.com>
2020-11-26 16:15:01 +0100

    Update data, split categories and add createdAt/updatedAt fields

--- server/controllers/poiController.js
+++ server/controllers/poiController.js
@@ -1,6 +1,6 @@
 module.exports = models => ({
     async findAll(req, res) {
-        const results = (await models.poi.findAll()).filter(poi => poi.categories.match(/Distribution alimentaire/));
+        const results = (await models.poi.findAll());
 
         return res.status(200).send(results);
     },
