0341edc431e0823d0e721a656da132e56c5be189
Anis Safine Laget <anis@users.noreply.github.com>
2020-12-01 15:46:56 +0100

    (580) Create a table for user access history

---
+++ db/migrations/000101-01-create-table-user_accesses.js
@@ -0,0 +1,85 @@
+module.exports = {
+
+    up: (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
+        transaction => queryInterface.createTable(
+            'user_accesses',
+            {
+                user_access_id: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                    primaryKey: true,
+                    autoIncrement: true,
+                },
+                token: {
+                    type: Sequelize.STRING,
+                    allowNull: false,
+                },
+                fk_user: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                sent_by: {
+                    type: Sequelize.INTEGER,
+                    allowNull: false,
+                },
+                created_at: {
+                    type: Sequelize.DATE,
+                    allowNull: false,
+                    defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+                },
+            },
+            {
+                transaction,
+            },
+        )
+            .then(() => queryInterface.addConstraint(
+                'user_accesses',
+                ['fk_user'],
+                {
+                    type: 'foreign key',
+                    name: 'fk_user_accesses_user',
+                    references: {
+                        table: 'users',
+                        field: 'user_id',
+                    },
+                    onUpdate: 'cascade',
+                    onDelete: 'cascade',
+                    transaction,
+                },
+            ))
+            .then(() => queryInterface.addConstraint(
+                'user_accesses',
+                ['sent_by'],
+                {
+                    type: 'foreign key',
+                    name: 'fk_user_accesses_admin',
+                    references: {
+                        table: 'users',
+                        field: 'user_id',
+                    },
+                    onUpdate: 'cascade',
+                    onDelete: 'cascade',
+                    transaction,
+                },
+            )),
+    ),
+
+    down: queryInterface => queryInterface.sequelize.transaction(
+        transaction => queryInterface.removeConstraint(
+            'user_accesses',
+            'fk_user_accesses_admin',
+            {
+                transaction,
+            },
+        )
+            .then(() => queryInterface.removeConstraint(
+                'user_accesses',
+                'fk_user_accesses_user',
+                {
+                    transaction,
+                },
+            ))
+            .then(() => queryInterface.dropTable('user_accesses', { transaction })),
+    ),
+
+};
