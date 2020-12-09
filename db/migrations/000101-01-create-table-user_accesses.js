5ec3bca038a2a0398a385603f5da3dbe54db1d52
Anis Safine Laget <anis@users.noreply.github.com>
2020-12-09 12:22:53 +0100

    (580) Migrer la base vers user_accesses

--- db/migrations/000101-01-create-table-user_accesses.js
+++ db/migrations/000101-01-create-table-user_accesses.js
@@ -16,7 +16,7 @@ module.exports = {
                 },
                 sent_by: {
                     type: Sequelize.INTEGER,
-                    allowNull: false,
+                    allowNull: true,
                 },
                 used_at: {
                     type: Sequelize.DATE,
@@ -63,6 +63,39 @@ module.exports = {
                     },
                     onUpdate: 'cascade',
                     onDelete: 'cascade',
+                    transaction,
+                },
+            ))
+            .then(() => queryInterface.sequelize.query(
+                `SELECT
+                    user_id AS fk_user,
+                    CASE
+                        WHEN fk_status <> 'new' THEN COALESCE(activated_by, created_by)
+                        ELSE NULL
+                    END AS sent_by,
+                    CASE
+                        WHEN fk_status <> 'new' THEN activated_on
+                        ELSE NULL
+                    END AS used_at,
+                    CASE
+                        WHEN fk_status <> 'new' THEN activated_on + (interval '1 day')
+                        ELSE last_activation_link_sent_on + (interval '7 days')
+                    END AS expires_at,
+                    CASE
+                        WHEN fk_status <> 'new' THEN COALESCE(last_activation_link_sent_on, created_at)
+                        ELSE last_activation_link_sent_on
+                    END AS created_at
+                FROM users
+                WHERE fk_status <> 'new' OR (fk_status = 'new' AND last_activation_link_sent_on IS NOT NULL)`,
+                {
+                    type: queryInterface.sequelize.QueryTypes.SELECT,
+                    transaction,
+                },
+            ))
+            .then(data => queryInterface.bulkInsert(
+                'user_accesses',
+                data,
+                {
                     transaction,
                 },
             )),
