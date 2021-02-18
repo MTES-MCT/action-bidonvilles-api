9eafb92986ecdffa488c257294fdc2973ed5429a
Christophe BENARD <ch.benard@gmail.com>
2021-02-18 19:06:16 +0100

    (709) Correction suite commentaires sur PR et màj wording

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -991,6 +991,12 @@ module.exports = models => ({
             }
         }
 
+        if (req.body.charte_agreement !== true) {
+            return res.status(400).send({
+                user_message: 'Vous devez vous engager à respecter les conditions d\'utilisation de la plateforme',
+            });
+        }
+
         if (req.body.confidentiality_agreement !== true) {
             return res.status(400).send({
                 user_message: 'Vous devez vous engager à ne pas diffuser les données de la plateforme',
