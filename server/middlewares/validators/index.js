6c5a3232e694e8dbe2ea09d66ca31b88321b24a4
Anis Safine Laget <anis@users.noreply.github.com>
2021-02-18 18:03:14 +0100

    (691) Ajout du validateur pour la modification de champs d'intervention

--- server/middlewares/validators/index.js
+++ server/middlewares/validators/index.js
@@ -3,6 +3,7 @@ const createTown = require('./createTown');
 const createUser = require('./createUser');
 const editTown = require('./editTown');
 const addShantytownActor = require('./shantytownActors/addShantytownActor');
+const updateShantytownActor = require('./shantytownActors/updateShantytownActor');
 
 module.exports = {
     createContact,
@@ -11,5 +12,6 @@ module.exports = {
     createUser,
     shantytownActors: {
         addShantytownActor,
+        updateShantytownActor,
     },
 };
