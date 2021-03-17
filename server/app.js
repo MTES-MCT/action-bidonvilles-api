dfe06b044e876e9493ce6ffcbe82f6e841c44d8b
gdestrem <gael.destrem@gmail.com>
2021-03-17 19:28:54 +0100

    Add more context on errors (path/method)

--- server/app.js
+++ server/app.js
@@ -2,30 +2,55 @@ const Sentry = require('@sentry/node');
 const loaders = require('#server/loaders');
 const { port } = require('#server/config');
 
+const sentryContextHandlers = (app) => {
+    // TODO : We should ideally use these handlers, but our async wrapping logic in routesLoaders cause issues
+    // => req.protocol is not defined / trust is not a function
+    // app.use(Sentry.Handlers.requestHandler());
+    // app.use(Sentry.Handlers.tracingHandler());
+
+    // Workaround to still have url in context
+    app.use(async (
+        req,
+        res,
+        next,
+    ) => {
+        Sentry.setContext('req', { path: req.path, method: req.method });
+        next();
+    });
+};
+
+
+const sentryErrorHandlers = (app) => {
+    // Report handled errors with next(error)
+    app.use(async (
+        err,
+        req,
+        res,
+        next,
+    ) => {
+        Sentry.captureException(err);
+
+        next();
+    });
+
+    // Report unhandled errors
+    // The error handler must be before any other error middleware
+    app.use(
+        Sentry.Handlers.errorHandler(),
+    );
+};
+
+
 module.exports = {
     async start() {
         // app
         const app = loaders.express();
 
+        sentryContextHandlers(app);
+
         loaders.routes(app);
 
-        // Report handled errors with next(error)
-        app.use(async (
-            err,
-            req,
-            res,
-            next,
-        ) => {
-            Sentry.captureException(err);
-
-            next();
-        });
-
-        // // Report unhandled errors
-        // // The error handler must be before any other error middleware
-        app.use(
-            Sentry.Handlers.errorHandler(),
-        );
+        sentryErrorHandlers(app);
 
         app.listen(port, () => {
             console.log(`Server is now running on port ${port}! :)`);
--- server/index.js
+++ server/index.js
@@ -13,14 +13,14 @@ app.start().then((expressApp) => {
             release: `rb-api@${packageJSON.version}`,
             environment: 'production',
             beforeSend(err) {
-                console.log('beforesend', err);
+                // console.log('sentry send', err);
                 return err;
             },
             integrations: [
                 // enable HTTP calls tracing
                 new Sentry.Integrations.Http({ tracing: true }),
                 // enable Postres calls tracing
-                // new Tracing.Integrations.Postgres(), // Add this integration
+                new Tracing.Integrations.Postgres(), // Add this integration
                 new Tracing.Integrations.Express({
                     // to trace all requests to the default router
                     app: expressApp,
