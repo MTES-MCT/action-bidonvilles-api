818b9fb9919fbdf0e123f565e9157a2cb0ea3f22
Anis Safine Laget <anis@users.noreply.github.com>
2021-03-30 12:22:34 +0200

    (642) Various improvements
    - support de Sentry
    - simplification du contrôleur : retrait de validations qui sont déjà faites dans le validateur, retrait d'une requête SQL qui est déjà faite dans le validateur, déplacement de la 200 à la fin de la fonction
    - clarification du mail : split des paramètres du template de "data" à "(guest, greeter)", ajout de liens et styling manquant (notamment sur le nom "Résorption-bidonvilles")
    - amélioration du validateur : utilisation de "bail" pour éviter la multiplication d'erreurs sur un même champ

--- server/controllers/inviteController.js
+++ server/controllers/inviteController.js
@@ -7,61 +7,40 @@ MAIL_TEMPLATES.invitation = require('#server/mails/invitation');
 
 const sendEmailsInvitations = async (guests, greeter) => {
     for (let i = 0; i < guests.length; i += 1) {
-        const data = {
-            first_name: guests[i].firstname,
-            last_name: guests[i].lastname,
+        const guest = {
+            first_name: guests[i].first_name,
+            last_name: guests[i].last_name,
             email: guests[i].email,
-            greeter_first_name: greeter.first_name,
-            greeter_last_name: greeter.last_name,
-            greeter_organization_name: greeter.organization.name,
         };
+
         try {
             // eslint-disable-next-line no-await-in-loop
-            await sendMail(data, MAIL_TEMPLATES.invitation(data));
+            await sendMail(guest, MAIL_TEMPLATES.invitation(guest, greeter));
         } catch (err) {
             // Ignore
         }
     }
 };
 
-module.exports = models => ({
-    async invite(req, res) {
+module.exports = () => ({
+    async invite(req, res, next) {
         const {
             guests,
-            greeter,
+            greeter_full: greeter,
         } = req.body;
 
         try {
-            const theGreeter = await models.user.findOneByEmail(greeter.email, { extended: true }, req.user);
-
-            if (Array.isArray(guests) && guests.length > 0) {
-                // envoi du mail d'invitation
-                try {
-                    await sendEmailsInvitations(guests, theGreeter);
-                } catch (err) {
-                    return res.status(400).send({
-                        error: {
-                            developer_message: 'Invitations could not be sent',
-                            user_message: 'Impossible d\'envoyer les invitations',
-                        },
-                    });
-                }
-                return res.status(200).send({});
-            }
+            await sendEmailsInvitations(guests, greeter);
         } catch (err) {
-            return res.status(400).send({
+            res.status(500).send({
                 error: {
-                    developer_message: 'greeter has not been found in database or a problem occured while fetching greeter from database',
-                    user_message: 'La personne a l\'initiative de l\'invitation n\'existe pas',
+                    developer_message: 'Invitations could not be sent',
+                    user_message: 'Impossible d\'envoyer les invitations',
                 },
             });
+            return next(err);
         }
 
-        return res.status(400).send({
-            error: {
-                developer_message: 'A problem occured while sending invitations',
-                user_message: 'Un problème n\'a pas permis d\'envoyer les invitations',
-            },
-        });
+        return res.status(200).send({});
     },
 });
