f7aec88de2f7aa79327a3ed6427a503673e30791
Anis Safine Laget <anis@users.noreply.github.com>
2021-04-28 17:00:59 +0200

    (904) Retrait d'un console.log

--- server/controllers/planController.js
+++ server/controllers/planController.js
@@ -464,7 +464,6 @@ module.exports = models => ({
             const plans = await models.plan.findAll(req.user);
             res.status(200).send(plans);
         } catch (error) {
-            console.log(error);
             res.status(500).send({
                 error: {
                     user_message: 'Une erreur est survenue lors de la récupération des données en base',
