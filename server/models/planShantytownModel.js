0d76705082ba500a7306a64365727e3bdcd52ef7
Anis Safine Laget <anis@users.noreply.github.com>
2021-05-04 16:22:51 +0200

    (931) PR feedback

--- server/models/planShantytownModel.js
+++ server/models/planShantytownModel.js
@@ -12,7 +12,7 @@ function serializePlan(planShantytown) {
         name: planShantytown.plan_name,
         goals: planShantytown.plan_goals,
         category: planShantytown.plan_category,
-        organization: {
+        operator: {
             name: planShantytown.org_name,
             abbreviation: planShantytown.org_abbreviation,
             id: planShantytown.org_id,
@@ -28,19 +28,20 @@ module.exports = database => ({
 
         return database.query(
             `SELECT 
-                fk_plan as plan_id,
-                fk_shantytown as shantytown_id,
+                ps.fk_plan as plan_id,
+                ps.fk_shantytown as shantytown_id,
                 plans2.name as plan_name,
                 plans2.goals as plan_goals,
                 plans2.fk_category as plan_category,
                 organizations.organization_id as org_id,
                 organizations.name as org_name,
                 organizations.abbreviation as org_abbreviation
-            FROM plan_shantytowns
-            LEFT JOIN plans2 on plans2.plan_id = fk_plan
-            LEFT JOIN users on users.user_id = plans2.created_by
+            FROM plan_shantytowns ps
+            LEFT JOIN plans2 on plans2.plan_id = ps.fk_plan
+            LEFT JOIN plan_operators ON plan_operators.fk_plan = ps.fk_plan
+            LEFT JOIN users on users.user_id = plan_operators.fk_user
             LEFT JOIN organizations on organizations.organization_id = users.fk_organization
-            WHERE fk_shantytown in (:ids)`,
+            WHERE ps.fk_shantytown in (:ids)`,
             {
                 type: database.QueryTypes.SELECT,
                 replacements: {
