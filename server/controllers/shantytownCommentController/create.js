d12fa4a5326ba50fae49a43e6999a1368ab458f9
Anis Safine Laget <anis@users.noreply.github.com>
2021-05-06 22:11:50 +0200

    (906) Retrait des requêtes SQL de createComment
    - déplacement de la requête SQL qui fetch la liste actualisée des commentaires de la couche contrôleur à modèle (en fait, utilisation de la fonction getComments() déjà disponible dans townModel)
    - ajout du champ "shantytown" à la serialization des commentaires dans townModel pour rester iso à la sérialisation qui était faite dans le contrôleur shantytownCommentController
    - amélioration de la gestion des erreurs dans le contrôleur : un try/catch pour la création de commentaire, un try/catch pour le fetch de commentaires

--- server/controllers/shantytownCommentController/create.js
+++ server/controllers/shantytownCommentController/create.js
@@ -2,7 +2,7 @@ const {
     sequelize,
     ShantytownComment: ShantyTownComments,
 } = require('#db/models');
-const serializeComment = require('./helpers/serializeComment');
+const { getComments } = require('#server/models/shantytownModel')(sequelize);
 
 module.exports = async (req, res, next) => {
     const {
@@ -11,7 +11,7 @@ module.exports = async (req, res, next) => {
         shantytown,
     } = req.body;
 
-    // add the step
+    // add the comment
     try {
         await ShantyTownComments.create({
             shantytown: shantytown.id,
@@ -19,47 +19,30 @@ module.exports = async (req, res, next) => {
             private: isPrivate,
             createdBy: req.user.id,
         });
-
-        const filterPrivateComments = !req.user.isAllowedTo('listPrivate', 'shantytown_comment');
-
-        const rawComments = await sequelize.query(
-            `SELECT
-                shantytown_comments.shantytown_comment_id AS "commentId",
-                shantytown_comments.fk_shantytown AS "shantytownId",
-                shantytown_comments.description AS "commentDescription",
-                shantytown_comments.created_at AS "commentCreatedAt",
-                shantytown_comments.created_by AS "commentCreatedBy",
-                shantytown_comments.private AS "commentPrivate",
-                users.first_name AS "userFirstName",
-                users.last_name AS "userLastName",
-                users.position AS "userPosition",
-                organizations.organization_id AS "organizationId",
-                organizations.name AS "organizationName",
-                organizations.abbreviation AS "organizationAbbreviation"
-            FROM shantytown_comments
-            LEFT JOIN users ON shantytown_comments.created_by = users.user_id
-            LEFT JOIN organizations ON users.fk_organization = organizations.organization_id
-            WHERE shantytown_comments.fk_shantytown = :id
-            ${filterPrivateComments === true ? 'AND private IS FALSE ' : ''}
-            ORDER BY shantytown_comments.created_at DESC`,
-            {
-                type: sequelize.QueryTypes.SELECT,
-                replacements: {
-                    id: shantytown.id,
-                },
-            },
-        );
-
-        return res.status(200).send({
-            comments: rawComments.map(serializeComment),
-        });
     } catch (e) {
         res.status(500).send({
             error: {
-                developer_message: e.message,
                 user_message: 'Une erreur est survenue dans l\'enregistrement de l\'étape en base de données',
             },
         });
         return next(e);
     }
+
+    // get an updated list of the comments
+    let comments;
+    try {
+        comments = await getComments(req.user, [shantytown.id], false);
+    } catch (e) {
+        res.status(500).send({
+            error: {
+                user_message: 'Le commentaire est enregistré mais la liste des commentaires n\'a pas pu être actualisée',
+            },
+        });
+        return next(e);
+    }
+
+    // respond
+    return res.status(200).send({
+        comments: comments[shantytown.id],
+    });
 };
