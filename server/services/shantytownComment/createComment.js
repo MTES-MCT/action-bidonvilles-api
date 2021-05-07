0943b493d80877aff04afb0ef835fbf83fc0c925
Anis Safine Laget <anis@users.noreply.github.com>
2021-05-07 18:29:19 +0200

    (906) Notification slack à chaque commentaire

--- server/services/shantytownComment/createComment.js
+++ server/services/shantytownComment/createComment.js
@@ -1,6 +1,7 @@
 const { sequelize } = require('#db/models');
 const { create: $createComment } = require('#server/models/shantytownComment');
 const { getComments: $getComments } = require('#server/models/shantytownModel')(sequelize);
+const { triggerNewComment: $triggerNewComment } = require('#server/utils/slack');
 const ServiceError = require('#server/errors/ServiceError');
 
 /**
@@ -13,43 +14,51 @@ function factory(dependencies) {
     const {
         createComment,
         getComments,
+        triggerNewComment,
     } = {
         ...{
             createComment: $createComment,
             getComments: $getComments,
+            triggerNewComment: $triggerNewComment,
         },
         ...dependencies,
     };
 
     /**
      * @param {Service_ShantytownComment_Create_CommentData} comment Commentaire
-     * @param {Number} shantytownId `shantytown_id` du site rattaché au commentaire
+     * @param {Model_Shantytown} shantytown site rattaché au commentaire
      * @param {Model_User} author Auteur du commentaire
      *
      * @returns {Array.<Model_ShantytownComment>}
      */
-    return async (comment, shantytownId, author) => {
+    return async (comment, shantytown, author) => {
         // create the new comment
         try {
             await createComment({
                 description: comment.description,
                 private: comment.private,
-                fk_shantytown: shantytownId,
+                fk_shantytown: shantytown.id,
                 created_by: author.id,
             });
         } catch (error) {
             throw new ServiceError('insert_failed', error);
         }
 
+        try {
+            await triggerNewComment(comment.description, shantytown, author);
+        } catch (error) {
+            // ignore
+        }
+
         // return the updated list of comments
         let comments;
         try {
-            comments = await getComments(author, [shantytownId], false);
+            comments = await getComments(author, [shantytown.id], false);
         } catch (error) {
             throw new ServiceError('fetch_failed', error);
         }
 
-        return comments[shantytownId];
+        return comments[shantytown.id];
     };
 }
 
