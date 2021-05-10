7a4409edb5b04e0ece86854693c6c9043843c50a
gdestrem <gael.destrem@gmail.com>
2021-05-10 18:33:17 +0200

    (931) Retrieve plan topics instead of goals

--- server/models/planShantytownModel.js
+++ server/models/planShantytownModel.js
@@ -10,7 +10,7 @@ function serializePlan(planShantytown) {
         plan_id: planShantytown.plan_id,
         shantytown_id: planShantytown.shantytown_id,
         name: planShantytown.plan_name,
-        goals: planShantytown.plan_goals,
+        topics: planShantytown.topics,
         category: planShantytown.plan_category,
         operator: {
             name: planShantytown.org_name,
@@ -31,17 +31,20 @@ module.exports = database => ({
                 ps.fk_plan as plan_id,
                 ps.fk_shantytown as shantytown_id,
                 plans2.name as plan_name,
-                plans2.goals as plan_goals,
                 plans2.fk_category as plan_category,
                 organizations.organization_id as org_id,
                 organizations.name as org_name,
-                organizations.abbreviation as org_abbreviation
+                organizations.abbreviation as org_abbreviation,
+                array_agg(topics.name) as topics
             FROM plan_shantytowns ps
             LEFT JOIN plans2 on plans2.plan_id = ps.fk_plan
+            LEFT JOIN plan_topics on plan_topics.fk_plan = ps.fk_plan
+            LEFT JOIN topics ON plan_topics.fk_topic = topics.uid
             LEFT JOIN plan_operators ON plan_operators.fk_plan = ps.fk_plan
             LEFT JOIN users on users.user_id = plan_operators.fk_user
             LEFT JOIN organizations on organizations.organization_id = users.fk_organization
-            WHERE ps.fk_shantytown in (:ids)`,
+            WHERE ps.fk_shantytown in (:ids)
+            GROUP BY ps.fk_plan, ps.fk_shantytown, plans2.name, plans2.fk_category, organizations.organization_id`,
             {
                 type: database.QueryTypes.SELECT,
                 replacements: {
