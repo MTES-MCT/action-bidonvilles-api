3271b2c88e5744d7e97d67539968315160718608
Anis Safine Laget <anis@users.noreply.github.com>
2021-05-17 15:51:19 +0200

    (906) Envoi d'un mail à chaque nouveau commentaire

--- test/suites/server/services/shantytownComment/createComment.spec.js
+++ test/suites/server/services/shantytownComment/createComment.spec.js
@@ -10,24 +10,34 @@ const { sequelize } = require('#db/models');
 const shantytownCommentModel = require('#server/models/shantytownComment');
 const shantytownModel = require('#server/models/shantytownModel')(sequelize);
 const slackUtils = require('#server/utils/slack');
+const userModel = require('#server/models/userModel')(sequelize);
+const mailService = require('#server/services/mailService');
 const ServiceError = require('#server/errors/ServiceError');
 
 const createComment = proxyquire('#server/services/shantytownComment/createComment', {
     '#server/models/shantytownModel': () => shantytownModel,
+    '#server/models/userModel': () => userModel,
 });
 const { serialized: fakeUser } = require('#test/utils/user');
+const { serialized: fakeComment } = require('#test/utils/shantytownComment');
 
 
 describe.only('services/shantytownComment', () => {
     const dependencies = {
         createComment: undefined,
+        findOneComment: undefined,
         getComments: undefined,
         triggerNewComment: undefined,
+        getShantytownWatchers: undefined,
+        sendMail: undefined,
     };
     beforeEach(() => {
         dependencies.createComment = sinon.stub(shantytownCommentModel, 'create');
+        dependencies.findOneComment = sinon.stub(shantytownCommentModel, 'findOne');
         dependencies.getComments = sinon.stub(shantytownModel, 'getComments');
         dependencies.triggerNewComment = sinon.stub(slackUtils, 'triggerNewComment');
+        dependencies.getShantytownWatchers = sinon.stub(userModel, 'getShantytownWatchers');
+        dependencies.sendMail = sinon.stub(mailService, 'send');
     });
     afterEach(() => {
         Object.values(dependencies).forEach(stub => stub && stub.restore());
@@ -48,8 +58,14 @@ describe.only('services/shantytownComment', () => {
 
                 // output data
                 output = {
+                    watchers: [fakeUser(), fakeUser({ id: 3 }), fakeUser({ id: 4 })],
+                    comment: fakeComment(),
                     commentList: [],
                 };
+
+                // createComment() retourne un id de commentaire
+                dependencies.createComment
+                    .resolves(1);
 
                 // getComments() retourne une liste de commentaires
                 dependencies.getComments
@@ -57,6 +73,16 @@ describe.only('services/shantytownComment', () => {
                     .resolves({
                         [input.shantytown.id]: output.commentList,
                     });
+
+                // findOneComment() retourne un commentaire
+                dependencies.findOneComment
+                    .withArgs(1)
+                    .resolves(output.comment);
+
+                // getShantytownWatchers() retourne une liste d'utilisateurs
+                dependencies.getShantytownWatchers
+                    .withArgs(input.shantytown.id, input.comment.private)
+                    .resolves(output.watchers);
 
                 response = await createComment(input.comment, input.shantytown, input.user);
             });
@@ -78,6 +104,13 @@ describe.only('services/shantytownComment', () => {
                 );
             });
 
+            it('envoie une notification mail', () => {
+                expect(dependencies.sendMail.callCount).to.be.eql(3);
+                expect(dependencies.sendMail).to.have.been.calledWithExactly('new_comment', output.watchers[0], undefined, [input.shantytown, output.comment]);
+                expect(dependencies.sendMail).to.have.been.calledWithExactly('new_comment', output.watchers[1], undefined, [input.shantytown, output.comment]);
+                expect(dependencies.sendMail).to.have.been.calledWithExactly('new_comment', output.watchers[2], undefined, [input.shantytown, output.comment]);
+            });
+
             it('collecte et retourne la liste des commentaires actualisés', async () => {
                 expect(response).to.be.eql(output.commentList);
             });
@@ -96,6 +129,8 @@ describe.only('services/shantytownComment', () => {
                         created_by: user.id,
                     })
                     .rejects(nativeError);
+
+                dependencies.getShantytownWatchers.resolves([]);
             });
 
             it('lance une exception de type ServiceError', async () => {
@@ -119,6 +154,7 @@ describe.only('services/shantytownComment', () => {
             beforeEach(() => {
                 dependencies.triggerNewComment.rejects(nativeError);
                 dependencies.getComments.resolves({ 1: [] });
+                dependencies.getShantytownWatchers.resolves([]);
             });
 
             it('aucune exception est lancée ', async () => {
@@ -141,6 +177,8 @@ describe.only('services/shantytownComment', () => {
                 dependencies.getComments
                     .withArgs(user, [1], false)
                     .rejects(nativeError);
+
+                dependencies.getShantytownWatchers.resolves([]);
             });
 
             it('lance une exception de type ServiceError', async () => {
