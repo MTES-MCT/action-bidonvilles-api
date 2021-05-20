fae30191ad8273bcb5acb965bac352179e0b468a
Anis Safine Laget <anis@users.noreply.github.com>
2021-05-20 17:57:46 +0200

    (906) Correction du nom de la vue qui conflict avec une autre table

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -823,7 +823,7 @@ module.exports = (database) => {
                 u.last_name
             FROM shantytown_watchers sw
             LEFT JOIN users u ON sw.fk_user = u.user_id
-            LEFT JOIN user_permissions up ON sw.fk_user = up.user_id
+            LEFT JOIN user_actual_permissions up ON sw.fk_user = up.user_id
             WHERE
                     sw.fk_shantytown = :shantytownId
                 AND up.fk_feature = 'listPrivate'
