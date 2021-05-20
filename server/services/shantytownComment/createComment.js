6c78709264df22838f2b80c418858174a266d50a
Anis Safine Laget <anis@users.noreply.github.com>
2021-05-20 12:12:28 +0200

    (906) Màj d'un service pour annuler le pattern factory

--- server/services/shantytownComment/createComment.js
+++ server/services/shantytownComment/createComment.js
@@ -1,66 +1,42 @@
 const { sequelize } = require('#db/models');
-const { create: $createComment } = require('#server/models/shantytownComment');
-const { getComments: $getComments } = require('#server/models/shantytownModel')(sequelize);
-const { triggerNewComment: $triggerNewComment } = require('#server/utils/slack');
+const shantytownCommentModel = require('#server/models/shantytownComment');
+const shantytownModel = require('#server/models/shantytownModel')(sequelize);
+const slackUtils = require('#server/utils/slack');
 const ServiceError = require('#server/errors/ServiceError');
 
 /**
- * @typedef {Object} Service_ShantytownComment_Create_CommentData
- * @property {String}  description Contenu du commentaire
- * @property {Boolean} private `true` si le commentaire est privé
+ * @param {Service_ShantytownComment_Create_CommentData} comment Commentaire
+ * @param {Model_Shantytown} shantytown site rattaché au commentaire
+ * @param {Model_User} author Auteur du commentaire
+ *
+ * @returns {Array.<Model_ShantytownComment>}
  */
+module.exports = async (comment, shantytown, author) => {
+    // create the new comment
+    try {
+        await shantytownCommentModel.create({
+            description: comment.description,
+            private: comment.private,
+            fk_shantytown: shantytown.id,
+            created_by: author.id,
+        });
+    } catch (error) {
+        throw new ServiceError('insert_failed', error);
+    }
 
-function factory(dependencies) {
-    const {
-        createComment,
-        getComments,
-        triggerNewComment,
-    } = {
-        ...{
-            createComment: $createComment,
-            getComments: $getComments,
-            triggerNewComment: $triggerNewComment,
-        },
-        ...dependencies,
-    };
+    try {
+        await slackUtils.triggerNewComment(comment.description, shantytown, author);
+    } catch (error) {
+        // ignore
+    }
 
-    /**
-     * @param {Service_ShantytownComment_Create_CommentData} comment Commentaire
-     * @param {Model_Shantytown} shantytown site rattaché au commentaire
-     * @param {Model_User} author Auteur du commentaire
-     *
-     * @returns {Array.<Model_ShantytownComment>}
-     */
-    return async (comment, shantytown, author) => {
-        // create the new comment
-        try {
-            await createComment({
-                description: comment.description,
-                private: comment.private,
-                fk_shantytown: shantytown.id,
-                created_by: author.id,
-            });
-        } catch (error) {
-            throw new ServiceError('insert_failed', error);
-        }
+    // return the updated list of comments
+    let comments;
+    try {
+        comments = await shantytownModel.getComments(author, [shantytown.id], false);
+    } catch (error) {
+        throw new ServiceError('fetch_failed', error);
+    }
 
-        try {
-            await triggerNewComment(comment.description, shantytown, author);
-        } catch (error) {
-            // ignore
-        }
-
-        // return the updated list of comments
-        let comments;
-        try {
-            comments = await getComments(author, [shantytown.id], false);
-        } catch (error) {
-            throw new ServiceError('fetch_failed', error);
-        }
-
-        return comments[shantytown.id];
-    };
-}
-
-module.exports = factory();
-module.exports.factory = factory;
+    return comments[shantytown.id];
+};
