450c7342b7ec0d8ea198fe46ad0033fe7eb8b72a
Anis Safine Laget <anis@users.noreply.github.com>
2021-05-25 11:44:40 +0200

    (906) Changement de wording pour le mail

--- server/mails/new_comment.js
+++ server/mails/new_comment.js
@@ -1,19 +1,18 @@
-const { toString: dateToString } = require('#server/utils/date');
 const signature = require('./signature');
 const { frontUrl } = require('#server/config');
-
 
 module.exports = (shantytown, comment) => ({
     Subject: `[ resorption-bidonvilles ] - Nouveau message site "${shantytown.usename}"`,
 
     TextPart: `Bonjour,
     
-    Un nouveau message a été publié par ${comment.createdBy.firstName} ${comment.createdBy.lastName.toUpperCase()} (${comment.createdBy.organization}) le ${dateToString(new Date(comment.createdAt * 1000))} sur le site situé ${shantytown.addressSimple} (${shantytown.departement.name}).
+    Un nouveau message a été publié par ${comment.createdBy.firstName} ${comment.createdBy.lastName.toUpperCase()} (${comment.createdBy.organization}) sur le site situé ${shantytown.addressSimple} (${shantytown.departement.name}).
 
-    "${comment.description}"
+    Voir le message : ${frontUrl}/site/${shantytown.id}#newComment
 
-    Accéder à la fiche du site : ${frontUrl}/site/${shantytown.id}
     Contacter ${comment.createdBy.firstName} ${comment.createdBy.lastName.toUpperCase()} : ${frontUrl}/annuaire/${comment.createdBy.organizationId}
+
+    En tant qu'intervenant·e sur ce site, vous êtes informé·e en temps réel de tout nouveau message.
 
     Cordialement,
 
@@ -33,13 +32,13 @@ module.exports = (shantytown, comment) => ({
                                 <td bgcolor="#ffffff">
                                     Bonjour,<br/>
                                     <br/>
-                                    Un nouveau message a été publié par ${comment.createdBy.firstName} ${comment.createdBy.lastName.toUpperCase()} (${comment.createdBy.organization}) le ${dateToString(new Date(comment.createdAt * 1000))} sur le site situé ${shantytown.addressSimple} (${shantytown.departement.name}).<br/>
+                                    Un nouveau message a été publié par ${comment.createdBy.firstName} ${comment.createdBy.lastName.toUpperCase()} (${comment.createdBy.organization}) sur le site situé ${shantytown.addressSimple} (${shantytown.departement.name}).<br/>
                                     <br/>
-                                    "${comment.description}"<br/>
-                                    <br/>
-                                    <a href="${frontUrl}/site/${shantytown.id}">Accéder à la fiche du site</a><br/>
+                                    <a href="${frontUrl}/site/${shantytown.id}#newComment">Voir le message</a><br/>
                                     <br/>
                                     <a href="${frontUrl}/annuaire/${comment.createdBy.organizationId}">Contacter ${comment.createdBy.firstName} ${comment.createdBy.lastName.toUpperCase()}</a><br/>
+                                    <br/>
+                                    En tant qu’intervenant·e sur ce site, vous êtes informé·e en temps réel de tout nouveau message.<br/>
                                     <br/>
                                     Cordialement,
                                 </td>
